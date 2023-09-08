d39dc2332488f28b3035f0907ba785160e0c93d3
Wenosi <hope.pw@bk.ru>
2023-09-08 19:56:40 +0300

    add task #1

---
+++ sem1task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int numA = int.Parse(Console.ReadLine() ?? "0");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+
+if (numA * numA == numB) 
+{ 
+    Console.WriteLine("First number it square second number");
+}
+else 
+{
+    Console.WriteLine("First number it not square second number");
+}
